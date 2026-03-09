38af2c6fbca27aacbaa288b0ddc6967a40d8a96a
rukarajinbelajar <maar1101dnd@gmail.com>
2026-03-09 22:25:06 +0700

    fungsi menentukan status berat badan

--- ConditionalStatement.cpp
+++ ConditionalStatement.cpp
@@ -5,3 +5,15 @@ float bmi(float bb, flloat tb)
 {
     return bb/(tb*tb)
 }
+
+string status(float r)
+{
+    if(r < 18.5)
+        return "Berat Badan Kurang";
+    else if(r < 25)
+        return "Berat Badan Normal";
+    else if(r < 30)
+        return "Berat Badan Kelebihan";
+    else
+        return "Obesitas";
+}
